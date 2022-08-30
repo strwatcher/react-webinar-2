6e15c972110440fce18a1d687b53ea4a2de1394b
StrWatcher <redmoonloon@outlook.com>
2022-08-30 15:41:38 +1000

    Article fix

--- src/app/article/index.js
+++ src/app/article/index.js
@@ -1,14 +1,18 @@
+import {useStore as useStoreEffector} from 'effector-react';
 import React from 'react';
 import {shallowEqual, useSelector as useSelectorRedux} from 'react-redux';
 import Layout from '../../components/layout';
 import Comments from '../../containers/article/comments';
 import ArticleDescription from '../../containers/article/desc';
+import HeadContainer from '../../containers/head';
 import ToolsContainer from '../../containers/tools';
 import TopContainer from '../../containers/top';
+import {useEffector} from '../../hooks/use-effector';
 
 function Article() {
-  // const {$data} = useEffector().article;
-  // const article = useStoreEffector($data);
+  const {models} = useEffector();
+  const {$data} = models.article;
+  const article = useStoreEffector($data);
 
   const select = useSelectorRedux(
     state => ({
@@ -22,7 +26,7 @@ function Article() {
   return (
     <Layout>
       <TopContainer />
-      {/* <HeadContainer title={article.title || ''} /> */}
+      <HeadContainer title={article.title || ''} />
       <ToolsContainer />
       <ArticleDescription />
       <Comments />
